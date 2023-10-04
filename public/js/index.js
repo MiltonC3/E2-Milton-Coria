6dbb93e8e87f9d9a31012c3dd744019796d10db3
MiltonC3 <miltoncoria03@gmail.com>
2023-10-04 15:59:33 -0300

    hice algunas notas

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 function menu(name, user) {
     const navUl = document.getElementById("nav");
 
+    // aqui renderizo el nav con los parametros que indique la funcion para la interfaz de usuario
     navUl.innerHTML = `
 <li class="header__li">
     <a id="inicio" href="/" class="header__a">Inicio</a>
@@ -19,6 +20,7 @@ function menu(name, user) {
 </li>`;
 }
 
+// esta funcion tiene como tarea distinguir en cada pagina con un color distinto del texto en el nav para que sea mas interactivo
 function activeMenu(nav1, nav2, nav3, nav4, nav5) {
     const navUno = document.getElementById(nav1);
     const navDos = document.getElementById(nav2);
@@ -26,7 +28,6 @@ function activeMenu(nav1, nav2, nav3, nav4, nav5) {
     const navCuatro = document.getElementById(nav4);
     const navCinco = document.getElementById(nav5);
 
-    // Tu código aquí
     var rutaRelativa = window.location.pathname;
 
     rutaRelativa === `/` ? navUno.classList.add("header__a--active") : "";
@@ -44,8 +45,10 @@ function activeMenu(nav1, nav2, nav3, nav4, nav5) {
         : "";
 }
 
+// aqui llamo la funcion para que en cada pagina se realize lo pedido de distinguir en el nav en la ruta que me encuentro
 activeMenu("inicio", "reservar", "ayuda", "signin", "signup");
 
+// Y despues con el fetch interactuo con el servidor recibiendo cosas
 fetch("/userFront", {
     method: "GET",
 })
@@ -58,10 +61,13 @@ fetch("/userFront", {
     })
     .then((data) => {
         if (data.login !== undefined) {
+            // Aqui mediante el modulo userFront que como res tenia login, entonces aqui lo recibo y lo guardo en el local storage para que persistan los datos
             localStorage.setItem("user", JSON.stringify(data));
 
+            // Al local storage lo guardo en una constante y asi condicionar en ella
             const storage = JSON.parse(localStorage.getItem("user"));
 
+            // Analiza si el usuario ingresado es un cliente cambiara el menu y el enlace que llevara a la pagina cliente, en el caso que sea admin llevara a la pagina amdin desde el cliente
             if (
                 storage.login.correo !== "" &&
                 storage.login.correo !== "miltoncoria03@gmail.com"
@@ -75,105 +81,10 @@ fetch("/userFront", {
                 activeMenu("inicio", "reservar", "ayuda", "admin", "signout");
             }
         } else {
+            // si no se encuentra ningun usuario en la data que recibo de la db, vacio el local storage, para que asi tambien cuando le de en signout borre el local storage y cumpla la funcion de cerrar sesion
             localStorage.clear();
         }
     })
     .catch((error) => {
         console.error("Error:", error);
     });
-
-    // ************************************************
-{/* <button type="submit" formmethod="post" formaction="/ruta-del-servidor">Enviar</button> */}
-    // ************************************************
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const { value: pass } = Swal.fire({
-//         title: "Enter your password",
-//         input: "password",
-//         inputLabel: "Password",
-//         inputPlaceholder: "Enter your password",
-//         inputAttributes: {
-//             maxlength: 10,
-//             autocapitalize: "off",
-//             autocorrect: "off",
-//         },
-//         inputValidator: (value) => {
-//             if (!value) {
-//                 return "You need to write something!";
-//             } else if (value.length === 4) {
-//                 fetch("/alert", {
-//                     method: "POST",
-//                     headers: {
-//                         "Content-Type": "application/json",
-//                     },
-//                     body: JSON.stringify({ pin: value }),
-//                 })
-//                     .then((response) => {
-//                         if (!response.ok) {
-//                             Swal.fire(`correcto`);
-//                             return response.json();
-//                         } else {
-//                             throw new Error("Error al realizar la solicitud");
-//                         }
-//                     })
-//                     // .then((data) => {
-//                     //     console.log(data);
-//                     // })
-//                     // .catch((error) => {
-//                     //     // Manejar errores
-//                     //     console.error("Error:", error);
-//                     // });
-
-//                 return Swal.fire({
-//                     position: "top-end",
-//                     icon: "success",
-//                     title: "Your work has been saved",
-//                     showConfirmButton: false,
-//                     timer: 1500,
-//                 });
-//             } else {
-//                 return "You need to write something!";
-//             }
-//         },
-//     });
-// });
-
-// Swal.fire({
-//     title: "Submit your Github username",
-//     input: "text",
-//     inputAttributes: {
-//         autocapitalize: "off",
-//     },
-//     showCancelButton: true,
-//     confirmButtonText: "Look up",
-//     showLoaderOnConfirm: true,
-//     preConfirm: (input) => {
-//         return fetch('/alert',{
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//               },
-//             body: JSON.stringify({nombre:input})
-//         })
-//             .then((response) => {
-//                 if (!response.ok) {
-
-//                     return response.json();
-//                 } else{
-//                     throw new Error('Error al realizar la solicitud');
-//                 }
-
-//             })
-//             .catch((data) => {
-//                 Swal.showValidationMessage(`Request success`);
-//             });
-//     },
-//     // allowOutsideClick: () => !Swal.isLoading(),
-// }).then((result) => {
-//     if (result) {
-//         Swal.fire({
-//             title: `${result.value.login}'s avatar`,
-//             imageUrl: result.value.avatar_url,
-//         });
-//     }
-// });
